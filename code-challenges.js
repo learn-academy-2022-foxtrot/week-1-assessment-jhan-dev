f7b0e54c6fc88be942ba012972dfb720153206e9
Joseph <jhan.technology@gmail.com>
2022-09-09 16:18:10 -0700

    Finished code-challenges, coding-concepts, and interview-questions

--- code-challenges.js
+++ code-challenges.js
@@ -7,62 +7,120 @@
 // --------------------INSTRUCTOR EXAMPLE: Create the code that determines which of the two strings has more characters. Use the two sets of test variables provided.
 
 // Describe your process:
+    //First create an if else statement to check both fruit lengths to determine if the     condition of having a greater length than the other is met.
+
+    //If true, expected outcome is 'banana' for ex:1, 'cherry' for ex:2.
 
 // Set one:
-const fruit1 = "apple"
-const fruit2 = "banana"
-// Expected outcome: "banana"
+// const fruit1 = "apple"
+// const fruit2 = "banana"
+// // Expected outcome: "banana"
+
+// if (fruit1.length < fruit2.length) {
+//     console.log(fruit2)
+// }
+// else if (fruit1.length > fruit2.length) {
+//     console.log(fruit1)
+// }
 
 // Set two:
 // const fruit1 = "cherry"
 // const fruit2 = "kiwi"
 // Expected outcome: "cherry"
 
+// if (fruit1.length > fruit2.length) {
+//     console.log(fruit1)
+// }
+// else if (fruit1.length < fruit2.length) {
+//     console.log(fruit2)
+// }
+
 // --------------------1) Create the code that determines if a given number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below.
 
 // Describe your process:
+    //First create an if else statement to check if const temp is below 212, above 212, or = to 212.
 
-const temp = 42
+    //If below 212, console log `${temp} is below boiling point`
+    //else if above 212, console log `${temp} is above boiling point`
+    //else if equal to 212, console log `${temp} is at boiling point`
+
+// const temp = 42
 // Expected output: "42 is below boiling point"
+
+// if (temp < 212) {
+//     console.log(`${temp} is below boiling point`)
+// }
+
 
 // const temp = 350
 // Expected output: "350 is above boiling point"
 
+// if (temp > 212) {
+//     console.log(`${temp} is above boiling point`)
+// }
+
+
 // const temp = 212
 // Expected output: "212 is at boiling point"
+
+// if (temp === 212) {
+//     console.log(`${temp} is at boiling point`)
+// }
+
 
 // --------------------2) Create the code that will combine the two arrays and return the length using the test variables provided below.
 
 // Describe your process:
+    // First we have to concat both arrays into 1 array. After that, we will .length to get the new length of the array.
 
 const padres1984WorldSeriesRuns = [2, 5, 2, 2, 4]
 const padres1998WorldSeriesRuns = [6, 3, 5, 3]
 // Expected output: 9
 
+// console.log(padres1984WorldSeriesRuns.concat(padres1998WorldSeriesRuns).length)
+
+
 // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below.
 
 // Describe your process:
+    // We will want to use the reverse() method, but first we need to convert currentCohort from string to array. After applying reverse(), we will convert back to string w/ join().
 
 const currentCohort = "Foxtrot 2022"
 // Expected output: "2202 tortxoF"
 
+console.log('Expected: 2202 tortxoF ', `Actual: ${currentCohort.split('').reverse().join('')}`)
+
+
 // --------------------4) Create the code that will return the last index of the given value from the array using the test variables provided below.
 
 // Describe your process:
+    // We will use the lastIndexOf() method to get the index of the given value.
 
 const myNumbers = [13, 34, 42, 5, 5, 10, 27, 42, 10]
 const givenValue1 = 42
 // Expected output: 7
 
+    console.log('Expected: 7 ', `Actual: ${myNumbers.lastIndexOf(givenValue1)}`)
+
+
 const givenValue2 = 10
 // Expected output: 8
+
+    console.log('Expected: 8 ', `Actual: ${myNumbers.lastIndexOf(givenValue2)}`)
+
 
 // --------------------5) Create the code that will sort all the numbers in an array from largest to smallest. Use the test variables provided below.
 
 // Describe your process:
+    // We will use the sort() method and reverse() after to get the greatest to least array.
 
 const sanDiegoSummerTemperatures = [79, 80, 72, 73, 82, 77, 76]
 // Expected output: [82, 80, 79, 77, 76, 73, 72]
 
+    console.log('Expected: [82, 80, 79, 77, 76, 73, 72]', `Actual: ${sanDiegoSummerTemperatures.sort().reverse()}`)
+
+
 const sanDiegoWinterTemperatures = [59, 68, 62, 59, 66, 67, 66]
 // Expected output: [68, 67, 66, 66, 62, 59, 59]
+
+    console.log('Expected: [68, 67, 66, 66, 62, 59, 59]', `Actual: ${sanDiegoWinterTemperatures.sort().reverse()}`)
--- interview-questions.md
+++ interview-questions.md
@@ -8,50 +8,50 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 INSTRUCTOR EXAMPLE: What is a conditional statement?
 
-Your answer:
+Your answer:    Conditional statements are if else statements that require a specific condition to be met to produce a specific outcome.
 
-Researched answer:
+Researched answer:  Statements in computer programming languages that run specific courses of action if a certain criteria is met.
 
 1. What is git? What is the difference between git and Github?
 
-Your answer:
+Your answer:    Git is a version control software. Github is the cloud server version of a version control software.
 
-Researched answer:
+Researched answer:  Git is a open source version control software while Github is a web app service that hosts the majority of repositories with collab services and tools.
 
 2. Which JavaScript operators will return a Boolean value?
 
-Your answer:
+Your answer:    && and ||
 
-Researched answer:
+Researched answer:  Conditionals that have && and || will output a boolean after a conditional statement.
 
 3. What is an index? What is the difference between index and value?
 
-Your answer:
+Your answer:    An index is the location where the value is placed. The value is data that is stored inside an element.
 
-Researched answer:
+Researched answer:  Value is the current element, while Index is the location of where the value is stored.
 
 4. Compare and contrast arrays and strings.
 
-Your answer:
+Your answer:    arrays are containers that store multiple elements, while strings are text based characters such as letters and words in quotes.
 
-Researched answer:
+Researched answer:  Arrays are a collection of data stored in a memory, while Strings are text data type that interprets characters as is.
 
 5. Give a brief description of proper pair programming techniques. What are the roles of each person?
 
-Your answer:
+Your answer:    A good pair programming technique is a dual role of Driver and Navigator. The driver will do all the physical typing, editor usage, and hand eye coordination of writing code, while the navigator will provide guidance, psuedocoding verbage, problem solving, and/or research solutions to a problem.
 
-Researched answer:
+Researched answer:  The driver and navigator work together within 1 workstation. The driver does all the typing while the navigator analyzes all lines of code and observers for bugs, solutions, and outcomes.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. Higher Order Functions:
+1. Higher Order Functions:  Takes in one or more functions as arguements within another function.
 
-2. Jest:
+2. Jest:    Is a Javascript framework that tests for correctness of the codebase.
 
-3. Objects:
+3. Objects: Is an element with a collection of properties.
 
-4. Method:
+4. Method:  Are objects with built in functions and procedures.
 
-5. Classes:
+5. Classes: Are templates for creating objects.
